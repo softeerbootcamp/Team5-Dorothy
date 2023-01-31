a5fd26fcc23b63cd4d4d6c0294ee60fbbca11e66
pjs0418 <tony180322@gmail.com>
2023-01-31 18:35:06 +0900

    feat: MemberRepository 생성 insert 메서드 구현 TODO: Entity를 받아서 저장할 수 있도록 수정

---
+++ backend/src/main/java/dev/be/dorothy/member/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package dev.be.dorothy.member.repository;
+
+import dev.be.dorothy.member.Member;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Modifying
+    @Query("insert into member values (null, :memberId, :password, :name, :email, :image, :created_at, :updated_at, :is_deleted, :role );")
+    void insert(
+            @Param("memberId") String memberId,
+            @Param("password") String password,
+            @Param("name") String name,
+            @Param("email") String email,
+            @Param("image") String image,
+            @Param("created_at") String createdAt,
+            @Param("updated_at") String updatedAt,
+            @Param("is_deleted") boolean isDeleted,
+            @Param("role") String role);
+}
