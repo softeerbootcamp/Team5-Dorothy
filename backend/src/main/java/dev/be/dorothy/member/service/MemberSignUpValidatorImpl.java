9534a9a2155214bc8a670f3c2c853f7a2ad3ba95
pjs0418 <tony180322@gmail.com>
2023-02-01 11:33:36 +0900

    feat(BE): MemberSignUpValidatorImpl 클래스의 emailRegexValidate 메서드 구현 이메일 정규식과 인자로 받아온 이메일 값이 일치하는지 확인

--- backend/src/main/java/dev/be/dorothy/member/service/MemberSignUpValidatorImpl.java
+++ backend/src/main/java/dev/be/dorothy/member/service/MemberSignUpValidatorImpl.java
@@ -1,9 +1,19 @@
 package dev.be.dorothy.member.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MemberSignUpValidatorImpl implements MemberSignUpValidator {
     @Override
     public boolean emailRegexValidate(String email) {
-        return false;
+        boolean err = false;
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if(m.matches()) {
+            err = true;
+        }
+        return err;
     }
 
     @Override
