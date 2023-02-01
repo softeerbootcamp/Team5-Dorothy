3b533ab4b89a8c6f23f9ee0309519902938993ed
pjs0418 <tony180322@gmail.com>
2023-02-01 12:39:20 +0900

    feat(BE): MemberSignUpValidatorImpl ID 검증하는 메서드 구현 중복된 ID 검증 ID가 정규식에 부합하는지 검증 ID 중복 검증하는 메서드 추출 정규식 검증하는 메서드 추출 TODO: 메서드명 리팩토링, 반환 타입 리팩토링, 테스트 케이스 수정

--- backend/src/main/java/dev/be/dorothy/member/service/MemberSignUpValidatorImpl.java
+++ backend/src/main/java/dev/be/dorothy/member/service/MemberSignUpValidatorImpl.java
@@ -1,28 +1,51 @@
 package dev.be.dorothy.member.service;
 
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.member.repository.MemberRepository;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MemberSignUpValidatorImpl implements MemberSignUpValidator {
+    private final MemberRepository memberRepository;
+
+    public MemberSignUpValidatorImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public boolean emailRegexValidate(String email) {
-        boolean err = false;
         String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(email);
-        if(m.matches()) {
-            err = true;
-        }
-        return err;
+        validateRegex(regex, email);
+        return true;
     }
 
     @Override
     public boolean idValidate(String memberId) {
-        return false;
+        validateDuplicatedId(memberId);
+        String regex = "^[a-zA-Z]{1}[a-zA-Z0-9_]{4,11}$";
+        validateRegex(regex, memberId);
+        return true;
     }
 
     @Override
     public boolean passwordValidate(String password, String passwordCheck) {
         return false;
+    }
+
+    private void validateDuplicatedId(String memberId) {
+        int count = memberRepository.countByMemberId(memberId);
+
+        if(count >= 1) {
+            throw new BadRequestException("중복된 ID입니다.");
+        }
+    }
+
+    private void validateRegex(String regex, String target) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(target);
+        if(!m.matches()) {
+            throw new BadRequestException("조건에 부합하지 않습니다.");
+        }
     }
 }
