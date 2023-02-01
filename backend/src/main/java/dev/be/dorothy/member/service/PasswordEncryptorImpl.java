9cb3bc432c28528932c4e6b197e71856a2a97b16
solchan98 <dev.sol0427@gmail.com>
2023-02-01 17:26:43 +0900

    feat(BE): PasswordEncryptorImpl 해시 메서드 구현 SHA256 알고리즘을 사용하여 패스워드 해싱

--- backend/src/main/java/dev/be/dorothy/member/service/PasswordEncryptorImpl.java
+++ backend/src/main/java/dev/be/dorothy/member/service/PasswordEncryptorImpl.java
@@ -1,9 +1,10 @@
 package dev.be.dorothy.member.service;
 
-
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class PasswordEncryptorImpl implements PasswordEncryptor {
+
     private final MessageDigest messageDigest;
 
     public PasswordEncryptorImpl(MessageDigest messageDigest) {
@@ -12,7 +13,12 @@ public class PasswordEncryptorImpl implements PasswordEncryptor {
 
     @Override
     public String encrypt(String password) {
-        return null;
+        byte[] sha256Hash = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+        StringBuilder result = new StringBuilder();
+        for (byte hash : sha256Hash) {
+            result.append(Integer.toString((0xff & hash) + 0x100, 16).substring(1));
+        }
+        return result.toString();
     }
 
     @Override
