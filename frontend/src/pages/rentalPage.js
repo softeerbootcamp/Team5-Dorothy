2869631efbe603485c5af529b37c5d81d8e31ceb
miinjoo <minjoo9903@ewhain.net>
2023-02-02 21:50:54 +0900

    feat(FE): rentalPage 퍼블리싱

--- frontend/src/pages/rentalPage.js
+++ frontend/src/pages/rentalPage.js
@@ -1,4 +1,5 @@
 import AbstractView from './AbstractView.js';
+import placeData from '../components/placeData.js';
 
 export default class rentalPage extends AbstractView {
     constructor() {
@@ -6,7 +7,25 @@ export default class rentalPage extends AbstractView {
     }
     async getHtml() {
         return `
-        <h1>rental</h1>
+        <h2>공간대여 하실 건가요?</h2>
+        <div class="places-container">
+            <figure class="place-wrapper">
+                <img class="place-image" src="/src/assets/chair.svg"></img>
+                <figcaption class="place-name">휴식실</figcaption>
+            </figure>
+            <figure class="place-wrapper">
+                <img class="place-image" src="/src/assets/room.svg"></img>
+                <figcaption class="place-name">교실</figcaption>
+            </figure>
+            <figure class="place-wrapper">
+                <img class="place-image" src="/src/assets/soundless.svg"></img>
+                <figcaption class="place-name">방음부스</figcaption>
+            </figure>
+            <figure class="place-wrapper">
+                <img class="place-image" src="/src/assets/game.svg"></img>
+                <figcaption class="place-name">방음부스</figcaption>
+            </figure>
+        </div>
         `;
     }
 }
