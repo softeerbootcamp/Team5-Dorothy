6c61d0d3ba7d19178c01cb08ef0f423629056006
solchan98 <dev.sol0427@gmail.com>
2023-02-06 17:24:56 +0900

    feat(BE): MemberController signUp 세션 추가 회원가입 성공 후 세션을 발급하여 유저 정보 설정 부분 추가 구현

--- backend/src/main/java/dev/be/dorothy/member/controller/MemberController.java
+++ backend/src/main/java/dev/be/dorothy/member/controller/MemberController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
@@ -22,10 +25,16 @@ public class MemberController {
     }
 
     @PostMapping("")
-    public ResponseEntity<CommonResponse> signUp(@RequestBody SignUpReqDto signUpReqDto) {
+    public ResponseEntity<CommonResponse> signUp(
+            HttpServletRequest request,
+            @RequestBody SignUpReqDto signUpReqDto
+    ) {
         MemberResDto memberResDto = memberService.signUp(signUpReqDto);
         CommonResponse commonResponse = new CommonResponse(HttpStatus.CREATED, "회원가입이 완료되었습니다.", memberResDto);
-        // TODO: 세션 등록 및 쿠키 발급
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute("member", memberResDto);
+
         return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
     }
 }
