206afc131a4023fc1ead3a3849e6d4de62ba8ba2
i5046821854 <o5046821854@gmail.com>
2023-02-06 15:56:31 +0900

    feat(BE): SecurityFilter 클래스 애노테이션 등록 DI를 위하여 @Component 애노테이션을 추가하고, 이후의 필터와의 순서를 맞추기 위해 @order 애노테이션을 추가

--- backend/src/main/java/dev/be/dorothy/auth/SecurityFilter.java
+++ backend/src/main/java/dev/be/dorothy/auth/SecurityFilter.java
@@ -3,12 +3,16 @@ package dev.be.dorothy.auth;
 import dev.be.dorothy.exception.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@Component
+@Order(1)
 public class SecurityFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityFilter.class);
@@ -23,4 +27,14 @@ public class SecurityFilter implements Filter {
         doFilter(request, response, chain);
     }
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        logger.info("filter init");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
