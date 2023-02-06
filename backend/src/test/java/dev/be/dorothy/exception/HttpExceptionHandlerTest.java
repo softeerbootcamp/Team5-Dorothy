9f23a6c9e05d1114b2e842c6a523f1360ea29540
solchan98 <dev.sol0427@gmail.com>
2023-02-06 15:04:47 +0900

    test(BE): BadRequestException handle 테스트 케이스 작성 BadRequest가 맞는지, 예외 메세지는 정확하게 오는지 확인하는 테스트 케이스 작성

---
+++ backend/src/test/java/dev/be/dorothy/exception/HttpExceptionHandlerTest.java
@@ -0,0 +1,34 @@
+package dev.be.dorothy.exception;
+
+import dev.be.dorothy.common.CommonResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("HttpExceptionHandler Test")
+public class HttpExceptionHandlerTest {
+
+    HttpExceptionHandler handler = new HttpExceptionHandler();
+
+    @Test
+    @DisplayName("BadRequest handle Test")
+    void badRequest() {
+        // given
+        BadRequestException exception = new BadRequestException("이것은 배드 리퀘스트야");
+
+        // when
+        ResponseEntity<CommonResponse> response = handler.handle(exception);
+
+        // then
+        assertAll(
+                () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST),
+                () -> assertThat(Objects.requireNonNull(response.getBody()).getMessage()).isEqualTo("이것은 배드 리퀘스트야")
+        );
+    }
+}
