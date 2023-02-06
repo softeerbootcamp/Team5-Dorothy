d964ad74321a771125ab9f08900f919be8f67af0
solchan98 <dev.sol0427@gmail.com>
2023-02-06 15:04:05 +0900

    feat(BE): HttpExceptionHandler 생성 및 BadRequestException handle 메서드 구현

---
+++ backend/src/main/java/dev/be/dorothy/exception/HttpExceptionHandler.java
@@ -0,0 +1,18 @@
+package dev.be.dorothy.exception;
+
+import dev.be.dorothy.common.CommonResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HttpExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<CommonResponse> handle(BadRequestException e) {
+        CommonResponse body = new CommonResponse(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+        return ResponseEntity
+                .badRequest()
+                .body(body);
+    }
+}
