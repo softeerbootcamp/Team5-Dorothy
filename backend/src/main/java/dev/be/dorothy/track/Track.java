0a0fbdb6d949c989b617d885d1a12af38a735220
pjs0418 <tony180322@gmail.com>
2023-02-07 16:54:52 +0900

    feat(BE): Track 클래스 구현 Track 테이블과 매핑되는 도메인 클래스 구현 TrackMember entity와 aggregation 형성

---
+++ backend/src/main/java/dev/be/dorothy/track/Track.java
@@ -0,0 +1,71 @@
+package dev.be.dorothy.track;
+
+import dev.be.dorothy.member.Member;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Track {
+    @Id
+    private Long idx;
+    private String name;
+    private String image;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+
+    public Track() {}
+
+    public Track(String name, String image) {
+        this.name = name;
+        this.image = image;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.isDeleted = false;
+    }
+
+    @MappedCollection(idColumn = "track_idx", keyColumn = "idx")
+    private final List<TrackMember> trackMembers = new ArrayList<>();
+
+    @Transient
+    private final List<Member> members = new ArrayList<>();
+
+    public void addTrackMember(Member member) {
+        trackMembers.add(new TrackMember(member.getIdx(), member.getRole(), LocalDateTime.now(), false));
+    }
+
+    public List<Long> getMemberIds() {
+        return trackMembers.stream()
+                .map(TrackMember::getMemberIdx)
+                .collect(Collectors.toList());
+    }
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+}
