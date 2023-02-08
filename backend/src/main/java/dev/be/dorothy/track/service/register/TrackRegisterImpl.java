a582557d7c497da6308f54520a0060376534f12e
solchan98 <dev.sol0427@gmail.com>
2023-02-08 20:43:36 +0900

    feat(BE): 트랙 생성 관련 기능을 관리하는 TrackRegister을 구현하는 TrackRegisterImpl 생성 TODO: create 메서드 상세 구현 하기

---
+++ backend/src/main/java/dev/be/dorothy/track/service/register/TrackRegisterImpl.java
@@ -0,0 +1,11 @@
+package dev.be.dorothy.track.service.register;
+
+import dev.be.dorothy.member.MemberRole;
+import dev.be.dorothy.track.service.TrackResDto;
+
+public class TrackRegisterImpl implements TrackRegister {
+    @Override
+    public TrackResDto create(Long memberIdx, String name, MemberRole role) {
+        return null;
+    }
+}
