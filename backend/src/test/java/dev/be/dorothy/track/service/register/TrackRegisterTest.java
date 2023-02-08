9e628f0ee111704e77066f03698dcf608a87f1fb
solchan98 <dev.sol0427@gmail.com>
2023-02-08 21:36:37 +0900

    test(BE): TrackRegister 테스트 케이스 수정 및 추가 작성 기존 create 성공 테스트 케이스 제거 트랙 생성은 운영진 권한만 가능하기에 권한이 Member 또는 Super Admin인 경우에 대한 테스트 케이스 작성

--- backend/src/test/java/dev/be/dorothy/track/service/register/TrackRegisterTest.java
+++ backend/src/test/java/dev/be/dorothy/track/service/register/TrackRegisterTest.java
@@ -1,54 +1,39 @@
 package dev.be.dorothy.track.service.register;
 
-import dev.be.dorothy.member.Member;
+import dev.be.dorothy.exception.ForbiddenException;
 import dev.be.dorothy.member.MemberRole;
-import dev.be.dorothy.track.Track;
-import dev.be.dorothy.track.repository.TrackRepository;
-import dev.be.dorothy.track.service.TrackResDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("TrackRegister Test")
 public class TrackRegisterTest {
 
-    @Mock
-    TrackRepository trackRepository;
-
     @InjectMocks
     TrackRegisterImpl trackRegister;
 
-    @Mock
-    Member member;
+    @Test
+    @DisplayName("권한이 운영진이 아닌 경우 (멤버인 경우) 403 예외를 던지는지 테스트")
+    void createTrackWhenMember() {
+        // given when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegister.create(1L, "lucas", MemberRole.MEMBER)
+        );
+    }
 
     @Test
-    @DisplayName("트랙 생성 성공하여 정상적으로 TrackResDto 응답하는지 테스트")
-    void createTrack() {
-        // given
-        given(member.getIdx()).willReturn(1L);
-        given(member.getRole()).willReturn(MemberRole.MEMBER);
-
-        Track track = new Track("code-squad", "");
-        track.addTrackMember(member);
-        given(trackRepository.save(any())).willReturn(track);
-
-        // when
-        TrackResDto trackResDto = trackRegister.create(1L, "lucas", MemberRole.MEMBER);
-
-        // then
-        assertAll(
-                () -> assertThat(trackResDto.getName()).isEqualTo("code-squad"),
-                () -> assertThat(trackResDto.getRole()).isEqualTo(MemberRole.MEMBER)
+    @DisplayName("권한이 운영진이 아닌 경우 (슈퍼 어드민인 경우) 403 예외를 던지는지 테스트")
+    void createTrackWhenSuperAdmin() {
+        // given when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegister.create(1L, "lucas", MemberRole.SUPER_ADMIN)
         );
     }
 }
