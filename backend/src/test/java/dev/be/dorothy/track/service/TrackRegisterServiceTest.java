358d05c1e26afc84ee2d938e03badc99bf0c1a6c
solchan98 <dev.sol0427@gmail.com>
2023-02-09 11:47:23 +0900

    test(BE): TrackRegisterService 테스트 케이스 추가 트랙 가입 시, 초대 코드가 일치하지 않는 경우 403 예외를 던지는지 확인하는 테스트 케이스 작성

--- backend/src/test/java/dev/be/dorothy/track/service/TrackRegisterServiceTest.java
+++ backend/src/test/java/dev/be/dorothy/track/service/TrackRegisterServiceTest.java
@@ -1,8 +1,7 @@
-package dev.be.dorothy.track.service.register;
+package dev.be.dorothy.track.service;
 
 import dev.be.dorothy.exception.ForbiddenException;
 import dev.be.dorothy.member.MemberRole;
-import dev.be.dorothy.track.service.TrackRegisterServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("TrackRegisterService Test")
 public class TrackRegisterServiceTest {
 
     @InjectMocks
-    TrackRegisterServiceImpl trackRegister;
+    TrackRegisterServiceImpl trackRegisterService;
 
     @Test
     @DisplayName("권한이 운영진이 아닌 경우 (멤버인 경우) 403 예외를 던지는지 테스트")
@@ -24,7 +25,7 @@ public class TrackRegisterServiceTest {
         // given when then
         assertThrows(
                 ForbiddenException.class,
-                () -> trackRegister.create(1L, "lucas", MemberRole.MEMBER)
+                () -> trackRegisterService.create(1L, "lucas", MemberRole.MEMBER)
         );
     }
 
@@ -34,7 +35,24 @@ public class TrackRegisterServiceTest {
         // given when then
         assertThrows(
                 ForbiddenException.class,
-                () -> trackRegister.create(1L, "lucas", MemberRole.SUPER_ADMIN)
+                () -> trackRegisterService.create(1L, "lucas", MemberRole.SUPER_ADMIN)
+        );
+    }
+
+    @Test
+    @DisplayName("트랙 가입 시, 초대 코드가 일치하지 않는 경우 테스트")
+    void joinWithInvalidJoinCode() {
+        // given
+        long trackIdx = 1L;
+        long memberIdx = 99L;
+        String joinCode = "123456";
+        TrackCodeManagerService trackCodeManagerService = mock(TrackCodeManagerService.class);
+        given(trackCodeManagerService.read(Long.toString(trackIdx))).willReturn("a1b2c3");
+
+        // when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegisterService.join(trackIdx, memberIdx, joinCode)
         );
     }
 }
