79bf2c617ac8cae5bafd08b92f1866c50155a054
miinjoo <minjoo9903@ewhain.net>
2023-02-09 11:51:06 +0900

    feat(FE): 성공/실패 모달 생성

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -1,1 +1,25 @@
-export default modal;
+const modal = (code, message) => {
+    const ModalTamplate = `
+    <div class='modal-wrapper'>
+    ${code == 'CREATED' ? successModal(message) : failModal(message)}
+    </div>
+    `;
+    return ModalTamplate;
+};
+
+const failModal = (text) => {
+    const failModalTamplate = `
+    <i class="fa-solid fa-circle-check"></i>
+    <p>${text}</p>
+    `;
+    return failModalTamplate;
+};
+
+const successModal = (text) => {
+    const successModalTamplate = `
+    <i class="fa-solid fa-circle-exclamation"></i>
+    <p>${text}</p>`;
+    return successModalTamplate;
+};
+
+export { modal };
