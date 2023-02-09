28dd171aaedc10f5927e10ac6d906a8b5f1e11be
pjs0418 <tony180322@gmail.com>
2023-02-09 12:24:15 +0900

    test(BE): TrackCodeManagerServiceImplTest 클래스의 메서드 구현 초대 코드를 잘 조회하는지 테스트하는 read 테스트 코드 작성

--- backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
+++ backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class TrackCodeManagerServiceImplTest {
@@ -24,5 +25,15 @@ public class TrackCodeManagerServiceImplTest {
         String code = trackCodeManagerImpl.store("1");
 
         assertThat(code.length()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("초대 코드 조회 테스트")
+    void read() {
+        given(redisDao.getValues("1")).willReturn("ab123c");
+
+        String code = trackCodeManagerImpl.read("1");
+
+        assertThat(code).isEqualTo("ab123c");
     }
 }
