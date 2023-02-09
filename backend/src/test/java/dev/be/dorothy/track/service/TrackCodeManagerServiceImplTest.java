30edc148e7b2a1e5b5b1eedca22b5bab5b4e7e32
pjs0418 <tony180322@gmail.com>
2023-02-09 12:11:02 +0900

    test(BE): TrackCodeManagerServiceImplTest 수정 Mockito 사용해서 RedisDao 의존성 주입 받도록 구현

--- backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
+++ backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
@@ -1,17 +1,27 @@
 package dev.be.dorothy.track.service;
 
+import dev.be.dorothy.redis.RedisDao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class TrackCodeManagerServiceImplTest {
-    TrackCodeManagerServiceImpl trackCodeManagerImpl = new TrackCodeManagerServiceImpl();
+    @Mock
+    RedisDao redisDao;
+
+    @InjectMocks
+    TrackCodeManagerServiceImpl trackCodeManagerImpl;
 
     @Test
     @DisplayName("초대 코드 발급 테스트")
     void store() {
-        String code = trackCodeManagerImpl.store();
+        String code = trackCodeManagerImpl.store("1");
 
         assertThat(code.length()).isEqualTo(6);
     }
