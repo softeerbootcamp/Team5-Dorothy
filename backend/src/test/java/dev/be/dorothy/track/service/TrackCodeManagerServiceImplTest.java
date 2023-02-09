df1f6dc1b4c166ca94d1726c5e2472e90e3aacfe
pjs0418 <tony180322@gmail.com>
2023-02-09 15:17:44 +0900

    test(BE): TrackCodeManagerServiceImplTest에서 초대 코드 조회 실패 케이스 테스트 코드 작성

--- backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
+++ backend/src/test/java/dev/be/dorothy/track/service/TrackCodeManagerServiceImplTest.java
@@ -1,5 +1,6 @@
 package dev.be.dorothy.track.service;
 
+import dev.be.dorothy.exception.BadRequestException;
 import dev.be.dorothy.redis.RedisDao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,12 +30,23 @@ public class TrackCodeManagerServiceImplTest {
     }
 
     @Test
-    @DisplayName("초대 코드 조회 테스트")
-    void read() {
+    @DisplayName("초대 코드 조회 테스트 - 성공 케이스")
+    void readSuccessTest() {
         given(redisDao.getValues("1")).willReturn("ab123c");
 
         String code = trackCodeManagerImpl.read("1");
 
         assertThat(code).isEqualTo("ab123c");
+    }
+
+    @Test
+    @DisplayName("초대 코드 조회 테스트 - 실패 케이스")
+    void readFailTest() {
+        given(redisDao.getValues("1")).willReturn(null);
+
+        BadRequestException badRequestException =
+                assertThrows(BadRequestException.class, () -> trackCodeManagerImpl.read("1"));
+
+        assertThat(badRequestException.getMessage()).isEqualTo("잘못된 요청입니다.");
     }
 }
