57832b8292184442450552bc8c0a2dd7e6e0cdd6
pjs0418 <tony180322@gmail.com>
2023-02-09 11:46:05 +0900

    test(BE): RedisDaoTest 생성 Redis에 값이 잘 저장되는지 테스트하는 코드 작성

---
+++ backend/src/test/java/dev/be/dorothy/redis/RedisDaoTest.java
@@ -0,0 +1,28 @@
+package dev.be.dorothy.redis;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {RedisConfig.class, RedisDao.class})
+public class RedisDaoTest {
+    @Autowired
+    private RedisDao redisDao;
+
+    @AfterEach
+    void clear() {
+        redisDao.delete("*");
+    }
+
+    @Test
+    @DisplayName("Redis Test")
+    void redisTest() {
+        redisDao.setValues("key", "value");
+        String value = redisDao.getValues("key");
+        assertThat(value).isEqualTo("value");
+    }
+}
