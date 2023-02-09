5377bf108554f176369c6bfcba9ef3a383bc1e66
solchan <64524916+solchan98@users.noreply.github.com>
2023-02-09 15:22:19 +0900

    트랙 참여 기능 구현 (#60)
    * chore(BE): TrackCodeManagerService 인터페이스 추가
    준수님 작업 부분을 인터페이스로 정의하여 미리 작업하기 위해 추가
    
    * refactor(BE): TrackRegister 패키지 구조 수정
    TrackRegister 패키지를 track/service 에서 공통 관리 및 클래스 이름 변경
    
    * feat(BE): TrackRegisterService join 메서드 정의
    TrackRegisterService join 메서드 정의
    TrackRegisterServiceImpl join 메서드 구현(로직 구현 X)
    
    * test(BE): TrackRegisterService 테스트 케이스 추가
    트랙 가입 시, 초대 코드가 일치하지 않는 경우 403 예외를 던지는지 확인하는 테스트 케이스 작성
    
    * test(BE): TrackRegisterService 테스트 케이스 추가
    트랙 가입 시, 트랙이 존재하지 않는 경우에 대한 테스트 케이스 작성
    
    * feat(BE): TrackRegisterService join 메서드 수정
    join 메서드의 파라미터 중 trackMemberIdx를 memberIdx로 변경
    
    * test(BE): TrackRegisterService 테스트 케이스 수정
    trackRepository를 전역 mock 객체로 변경
    
    * feat(BE): TrackRegisterService join 메서드 구현
    track이 존재하지 않는 경우 400 예외 처리
    전달받은 joinCode와 track의 joinCode가 다른 경우 403 처리
    예외가 발생하지 않는다면 생성 처리

---
+++ backend/src/test/java/dev/be/dorothy/track/service/TrackRegisterServiceTest.java
@@ -0,0 +1,88 @@
+package dev.be.dorothy.track.service;
+
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.exception.ForbiddenException;
+import dev.be.dorothy.member.MemberRole;
+import dev.be.dorothy.track.Track;
+import dev.be.dorothy.track.repository.TrackRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("TrackRegisterService Test")
+public class TrackRegisterServiceTest {
+
+    @Mock
+    TrackRepository trackRepository;
+
+    @Mock
+    TrackCodeManagerService trackCodeManagerService;
+
+    @InjectMocks
+    TrackRegisterServiceImpl trackRegisterService;
+
+    @Test
+    @DisplayName("권한이 운영진이 아닌 경우 (멤버인 경우) 403 예외를 던지는지 테스트")
+    void createTrackWhenMember() {
+        // given when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegisterService.create(1L, "lucas", MemberRole.MEMBER)
+        );
+    }
+
+    @Test
+    @DisplayName("권한이 운영진이 아닌 경우 (슈퍼 어드민인 경우) 403 예외를 던지는지 테스트")
+    void createTrackWhenSuperAdmin() {
+        // given when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegisterService.create(1L, "lucas", MemberRole.SUPER_ADMIN)
+        );
+    }
+
+    @Test
+    @DisplayName("트랙 가입 시, 초대 코드가 일치하지 않는 경우 테스트")
+    void joinWithInvalidJoinCode() {
+        // given
+        long trackIdx = 1L;
+        long memberIdx = 99L;
+        String joinCode = "123456";
+        given(trackRepository.findById(trackIdx)).willReturn(Optional.of(mock(Track.class)));
+        given(trackCodeManagerService.read(String.valueOf(trackIdx))).willReturn("a1b2c3");
+
+        // when then
+        assertThrows(
+                ForbiddenException.class,
+                () -> trackRegisterService.join(trackIdx, memberIdx, joinCode)
+        );
+    }
+
+    @Test
+    @DisplayName("트랙 가입 시, 트랙이 존재하지 않는 경우")
+    void joinWhenTrackDoesNotExist() {
+        // given
+        long trackIdx = 1L;
+        given(trackRepository.findById(trackIdx)).willReturn(Optional.empty());
+
+        // when
+        BadRequestException exception = assertThrows(
+                BadRequestException.class,
+                () -> trackRegisterService.join(trackIdx, 99L, "123456")
+        );
+
+        // then
+        assertThat(exception.getMessage()).isEqualTo("트랙정보가 유효하지 않습니다.");
+    }
+}
