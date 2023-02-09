fc6bd2184149a159e9c963ae716e74e78e67c814
miinjoo <minjoo9903@ewhain.net>
2023-02-09 20:17:56 +0900

    fix: 로그인 post 수정

--- frontend/src/apis/services/userservice.js
+++ frontend/src/apis/services/userservice.js
@@ -12,7 +12,7 @@ const UserService = {
     //로그인
     getUser: (id, password) =>
         axios.post(BASE_URL + '/member/login', {
-            username: id,
+            memberId: id,
             password: password,
         }),
 
