d3d0357f4569223fc2306ca323b2b4b23bb07398
i5046821854 <o5046821854@gmail.com>
2023-02-09 10:17:35 +0900

    feat(BE): SecurityFilterTest 미구현부 구현 authenticationFilter의 필터링 기능 구현이 완성되어 해당 사항을 테스트할 수 있도록 테스트 코드 작성

--- backend/src/test/java/dev/be/dorothy/auth/SecurityFilterTest.java
+++ backend/src/test/java/dev/be/dorothy/auth/SecurityFilterTest.java
@@ -1,15 +1,12 @@
 package dev.be.dorothy.auth;
 
 import dev.be.dorothy.auth.authentication.UsernameAndPasswordTokenProvider;
-import dev.be.dorothy.exception.BadRequestException;
 import dev.be.dorothy.member.service.PasswordEncryptor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(AuthenticationFilter.class)
@@ -32,6 +28,10 @@ class SecurityFilterTest {
     PasswordEncryptor passwordEncryptor;
     @InjectMocks
     AuthenticationFilter authenticationFilter ;
+    @MockBean
+    AuthenticationManager authenticationManager;
+    @InjectMocks
+    LoginFilter loginFilter;
 
     @Autowired
     MockMvc mockMvc;
@@ -43,6 +43,7 @@ class SecurityFilterTest {
     void testSetUp(){
         mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .addFilter(authenticationFilter)
+                .addFilter(loginFilter)
                 .build();
         session.setAttribute("JSESSIONID", "testJSESSIONID");
     }
@@ -55,7 +56,7 @@ class SecurityFilterTest {
     @Test
     @DisplayName("컨트롤러 접근 테스트 - 세션 없이 접근할 때")
     void accessWithoutSession(){
-//        BadRequestException exception = assertThrows(BadRequestException.class, () -> mockMvc.perform(MockMvcRequestBuilders.get("/")));
-//        assertThat(exception.getMessage()).isEqualTo("세션이 존재하지 않습니다.");
+        //ToDo : authorizationFilter 완성 후 접근 권한 관련 사항 검사 로직 추가
+        assertDoesNotThrow(()->mockMvc.perform(MockMvcRequestBuilders.get("/").session(session)));
     }
 }
