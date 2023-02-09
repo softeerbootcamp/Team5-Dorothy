896cf0e49ab7e1e32f6a731f0bac531ffe62e5b8
i5046821854 <o5046821854@gmail.com>
2023-02-09 10:54:17 +0900

    feat(BE): authenticationManager에서 로그인 response 형식 변경 memberController에서 commonResponse 형태로 반환할 수 있도록 핸들링 메소드 로직 수정

--- backend/src/main/java/dev/be/dorothy/auth/LoginFilter.java
+++ backend/src/main/java/dev/be/dorothy/auth/LoginFilter.java
@@ -1,14 +1,18 @@
 package dev.be.dorothy.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.be.dorothy.common.CommonResponse;
+import dev.be.dorothy.exception.BadRequestException;
 import dev.be.dorothy.member.service.LoginReqDto;
 import dev.be.dorothy.member.service.MemberResDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -35,8 +39,12 @@ public class LoginFilter implements Filter {
         String url = httpReq.getRequestURI();
         logger.info(url);
         if(url.equals("/member/login")){
-            MemberResDto memberResDto = attemptAuthentication(httpReq);
-            writeResponse(httpReq, response, memberResDto);
+            try {
+                MemberResDto memberResDto = attemptAuthentication(httpReq);
+                writeSuccessResponse(httpReq, response, memberResDto);
+            }catch (BadRequestException e) {
+                writeFailResponse(response, e.getMessage());
+            }
             return;
         }
         chain.doFilter(request, response);
@@ -47,14 +55,29 @@ public class LoginFilter implements Filter {
         Filter.super.destroy();
     }
 
-    private void writeResponse(HttpServletRequest httpReq, ServletResponse response, MemberResDto memberResDto) throws IOException {
+    private void writeSuccessResponse(HttpServletRequest httpReq, ServletResponse response, MemberResDto memberResDto) throws IOException {
         HttpSession session = httpReq.getSession(true);
         session.setAttribute("member", memberResDto);
-        String result = objectMapper.writeValueAsString(memberResDto);
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "로그인에 성공하였습니다", memberResDto);
+        String result = objectMapper.writeValueAsString(commonResponse);
+        HttpServletResponse httpRes = (HttpServletResponse) response;
+        httpRes.setStatus(200);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
         response.getWriter().write(result);
     }
 
-    private MemberResDto attemptAuthentication(HttpServletRequest request) throws IOException {
+    private void writeFailResponse(ServletResponse response, String message) throws IOException {
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.UNAUTHORIZED, message, null);
+        String result = objectMapper.writeValueAsString(commonResponse);
+        HttpServletResponse httpRes = (HttpServletResponse) response;
+        httpRes.setStatus(401);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(result);
+    }
+
+    private MemberResDto attemptAuthentication(HttpServletRequest request) throws IOException, BadRequestException {
         LoginReqDto loginReqDto = objectMapper.readValue(request.getInputStream(), LoginReqDto.class);
         return authenticationManager.matchAuthentication(loginReqDto);
     }
