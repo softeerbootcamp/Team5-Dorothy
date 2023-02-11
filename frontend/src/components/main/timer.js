f7249791422346d4573bc7c6f6fa7fd283b208a5
miinjoo <minjoo9903@ewhain.net>
2023-02-11 21:40:18 +0900

    fix: 타이머 중앙 정렬 스타일변경

--- frontend/src/components/main/timer.js
+++ frontend/src/components/main/timer.js
@@ -1,13 +1,13 @@
 const timerForm = () => {
     const timer = `
     <div class="circle">
-        <svg width="210" viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
+        <svg width="225" viewBox="9 7 230 235" xmlns="http://www.w3.org/2000/svg">
             <g transform="translate(110,110)">
                 <circle r="100" class="e-c-base"/>
                 <g transform="rotate(-90)">
                     <circle r="100" class="e-c-progress"/>
                     <g id="e-pointer">
-                        <circle cx="100" cy="0" r="8" class="e-c-pointer"/>
+                        <circle cx="94" cy="0" r="8" class="e-c-pointer"/>
                     </g>
                 </g>
             </g>
