3a13c439c07c758ba6b6250a6dab46fae350bd03
miinjoo <minjoo9903@ewhain.net>
2023-02-12 11:35:17 +0900

    fix: 차트 x축 수정

--- frontend/src/components/main/chart/chart.js
+++ frontend/src/components/main/chart/chart.js
@@ -1,5 +1,7 @@
 import { daysOfWeek } from '../../calendar/constants';
 
+const axis_x = [0, 10, 20, 30, 40];
+
 const makeChart = () => {
     function getCurrentWeek() {
         const Today = new Date();
@@ -19,21 +21,13 @@ const makeChart = () => {
         <div class="vertical_chart_box">
             <div class="chart_box">
                 <ul class="axis_y">
-                    <li class="item">
-                        0<span class="blind">명</span>
-                    </li>
-                    <li class="item">
-                        10<span class="blind">명</span>
-                    </li>
-                    <li class="item">
-                        20<span class="blind">명</span>
-                    </li>
-                    <li class="item">
-                        30<span class="blind">명</span>
-                    </li>
-                    <li class="item">
-                        40<span class="blind">명</span>
-                    </li>
+                ${axis_x
+                    .map((x) => {
+                        return `<li class="item">
+                        ${x}<span class="blind">명</span>
+                    </li>`;
+                    })
+                    .join('')}
                 </ul>
                 <ul class="axis_x">
                     ${getCurrentWeek()
