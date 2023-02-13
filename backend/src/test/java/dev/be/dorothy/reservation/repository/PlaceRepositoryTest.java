22b0d8be5cef981b5b4eb4c4c516159d552c84dd
i5046821854 <o5046821854@gmail.com>
2023-02-13 16:56:14 +0900

    test(BE): 모든 공간을 조회하는 기능을 테스트 두 가지의 places를 등록한 후 이를 findAllPlaces를 통하여 가져오고 해당 리턴값의 크기를 비교하는 것으로 테스트 코드를 작성

--- backend/src/test/java/dev/be/dorothy/reservation/repository/PlaceRepositoryTest.java
+++ backend/src/test/java/dev/be/dorothy/reservation/repository/PlaceRepositoryTest.java
@@ -1,13 +1,14 @@
 package dev.be.dorothy.reservation.repository;
 
 import dev.be.dorothy.reservation.Place;
+import dev.be.dorothy.reservation.service.PlaceResDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -18,11 +19,28 @@ class PlaceRepositoryTest {
     PlaceRepository placeRepository;
     
     @Test
-    @DisplayName("공간 등록 및 조회 기능 테스트")
+    @DisplayName("공간 등록 기능 테스트")
     void placeRegisterTest(){
         Place place = new Place("pot", "");
         Place savedPlace = placeRepository.save(place);
         Assertions.assertThat(savedPlace.getName()).isEqualTo("pot");
+    }
+
+    @Test
+    @DisplayName("findAll 커스텀 쿼리 테스트")
+    void findAllPlacesTest(){
+
+        //given
+        Place place1 = new Place("place1", "");
+        Place place2 = new Place("place2", "");
+        List<Place> places = List.of(place1, place2);
+        placeRepository.saveAll(places);
+
+        //when
+        List<PlaceResDto> allPlaces = placeRepository.findAllPlaces();
+
+        //then
+        Assertions.assertThat(allPlaces).hasSize(2);
     }
 
 }
