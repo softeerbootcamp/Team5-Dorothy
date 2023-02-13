9a89be796df031157fad8342b43b81b9fdffbab1
i5046821854 <o5046821854@gmail.com>
2023-02-13 16:58:59 +0900

    test(BE): placeController 클래스 생성 공유 공간을 등록하고 조회하기 위한 http request에 대하여 서비스 단으로 실행 흐름을 이어주기 위한 컨트롤러 메소드 구현

---
+++ backend/src/main/java/dev/be/dorothy/reservation/controller/PlaceController.java
@@ -0,0 +1,37 @@
+package dev.be.dorothy.reservation.controller;
+
+import dev.be.dorothy.common.CommonResponse;
+import dev.be.dorothy.reservation.Place;
+import dev.be.dorothy.reservation.service.PlaceResDto;
+import dev.be.dorothy.reservation.service.PlaceService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/places")
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @RequestMapping("")
+    public ResponseEntity<CommonResponse> registerPlace(@RequestParam("name") String name){
+        Place register = placeService.register(name);
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.CREATED, "공간 등록이 완료되었습니다.", register);
+        return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/list")
+    public ResponseEntity<CommonResponse> listAllPlaces(){
+        List<PlaceResDto> placeList = placeService.retrievePlaces();
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "공간 조회가 완료되었습니다.", placeList);
+        return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
+    }
+}
