b72845f673a0d8463fe84cb2a86225488516210a
pjs0418 <tony180322@gmail.com>
2023-02-13 17:56:17 +0900

    feat(BE): TrackRetrieveService 인터페이스에 getTrack 메서드 추가

--- backend/src/main/java/dev/be/dorothy/track/service/TrackRetrieveService.java
+++ backend/src/main/java/dev/be/dorothy/track/service/TrackRetrieveService.java
@@ -1,8 +1,10 @@
 package dev.be.dorothy.track.service;
 
+import dev.be.dorothy.track.Track;
+
 import java.util.List;
 
 public interface TrackRetrieveService {
-
     List<TrackResDto> retrieveTracks(Long userIdx);
+    Track getTrack(Long trackIdx);
 }
