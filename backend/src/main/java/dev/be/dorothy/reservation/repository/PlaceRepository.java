cd21f120e71a0138ea13306643ff548c986e6544
i5046821854 <o5046821854@gmail.com>
2023-02-13 16:54:48 +0900

    feat(BE): 등록된 모든 공간을 조회하는 repository 메소드 정의 PlaceResDto를 반환할 수 있도록 @query 어노테이션으로 직접 sql 문장 정의

--- backend/src/main/java/dev/be/dorothy/reservation/repository/PlaceRepository.java
+++ backend/src/main/java/dev/be/dorothy/reservation/repository/PlaceRepository.java
@@ -1,8 +1,14 @@
 package dev.be.dorothy.reservation.repository;
 
 import dev.be.dorothy.reservation.Place;
+import dev.be.dorothy.reservation.service.PlaceResDto;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface PlaceRepository extends CrudRepository<Place, Long> {
 
+    @Query("select idx, name, image, created_at, updated_at from place")
+    List<PlaceResDto> findAllPlaces();
 }
