9f630aee17ecb233097144646db521d88c929000
solchan98 <dev.sol0427@gmail.com>
2023-02-13 16:41:52 +0900

    feat(BE): Notice 도메인 구현 Notice 엔티티 작성

---
+++ backend/src/main/java/dev/be/dorothy/notice/Notice.java
@@ -0,0 +1,34 @@
+package dev.be.dorothy.notice;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table("notice")
+public class Notice {
+    @Id
+    private Long idx;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+
+    public Notice(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.isDeleted = false;
+    }
+
+    public static Notice of(String title, String content) {
+        return new Notice(title, content);
+    }
+}
