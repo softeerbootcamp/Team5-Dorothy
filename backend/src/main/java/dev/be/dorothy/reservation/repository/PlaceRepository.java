309ba532daaff9a56db1f289b5e5cdd6c2098d9c
i5046821854 <o5046821854@gmail.com>
2023-02-13 16:31:05 +0900

    feat(BE): PlaceRepository 생성 CRUDRepository를 extends하는 리포지토리 구성

---
+++ backend/src/main/java/dev/be/dorothy/reservation/repository/PlaceRepository.java
@@ -0,0 +1,8 @@
+package dev.be.dorothy.reservation.repository;
+
+import dev.be.dorothy.reservation.Place;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaceRepository extends CrudRepository<Place, Long> {
+
+}
