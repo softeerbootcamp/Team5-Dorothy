1f45815cdb4d4e46d37040b97eb15cf672ebc085
i5046821854 <o5046821854@gmail.com>
2023-02-13 22:07:07 +0900

    chore(BE): placeController 클래스 수정 placeService의 addPlace의 리턴값이 변경됨에 따라 controller에서 해당 메소드를 호출할 때도 dto타입으로 리턴 받도록 해당 사항 수정

--- backend/src/main/java/dev/be/dorothy/reservation/controller/PlaceController.java
+++ backend/src/main/java/dev/be/dorothy/reservation/controller/PlaceController.java
@@ -1,7 +1,6 @@
 package dev.be.dorothy.reservation.controller;
 
 import dev.be.dorothy.common.CommonResponse;
-import dev.be.dorothy.reservation.Place;
 import dev.be.dorothy.reservation.service.PlaceResDto;
 import dev.be.dorothy.reservation.service.PlaceService;
 
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/places")
+@RequestMapping("/api/v1/place")
 public class PlaceController {
 
     private final PlaceService placeService;
@@ -22,17 +21,17 @@ public class PlaceController {
     }
 
     @PostMapping("")
-    public ResponseEntity<CommonResponse> registerPlace(@RequestParam("name") String name){
-        Place register = placeService.register(name);
-        CommonResponse commonResponse = new CommonResponse(HttpStatus.CREATED, "공간 등록이 완료되었습니다.", register);
+    public ResponseEntity<CommonResponse> placeAdd(@RequestParam("name") String name){
+        PlaceResDto newPlace = placeService.addPlace(name);
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.CREATED, "공간 등록이 완료되었습니다.", newPlace);
         return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/list")
-    public ResponseEntity<CommonResponse> listAllPlaces(){
+    public ResponseEntity<CommonResponse> placeList(){
         List<PlaceResDto> placeList = placeService.retrievePlaces();
-        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "공간 조회가 완료되었습니다.", placeList);
-        return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "공간 조회에 성공하였습니다.", placeList);
+        return new ResponseEntity<>(commonResponse, HttpStatus.OK);
     }
 
 }
