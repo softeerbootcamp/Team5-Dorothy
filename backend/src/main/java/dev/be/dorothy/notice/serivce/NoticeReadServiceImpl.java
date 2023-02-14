d7274984b414081bbe9752ca67053d7c950e9648
solchan98 <dev.sol0427@gmail.com>
2023-02-14 10:59:27 +0900

    feat(BE): NoticeReadService 인터페이스 생성 및 NoticeReadServiceImpl 구현체 생성 NoticeReadService 인터페이스 생성 및 Notice 단일 및 전체 조회 메서드 정의 NoticeReadServiceImpl에 Notice 단일 및 전체 조회 메서드 빈 구현 작성

---
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeReadServiceImpl.java
@@ -0,0 +1,15 @@
+package dev.be.dorothy.notice.serivce;
+
+import java.util.List;
+
+public class NoticeReadServiceImpl implements NoticeReadService {
+    @Override
+    public NoticeResDto getNotice(Long noticeId) {
+        return null;
+    }
+
+    @Override
+    public List<NoticeResDto> getNotices() {
+        return null;
+    }
+}
