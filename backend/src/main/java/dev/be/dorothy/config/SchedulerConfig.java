1dcd857f2f045f053396ed3de25bd4ed57225959
solchan98 <dev.sol0427@gmail.com>
2023-02-14 12:12:22 +0900

    feat(BE): SchedulerConfig 추가 구성 ExampleScheduler를 적용하는 부분 추가 현재는 주석 처리하였으며, 확인이 필요한 경우에 주석 해제하여 적용 가능

--- backend/src/main/java/dev/be/dorothy/config/SchedulerConfig.java
+++ backend/src/main/java/dev/be/dorothy/config/SchedulerConfig.java
@@ -1,5 +1,7 @@
 package dev.be.dorothy.config;
 
+import dev.be.dorothy.common.scheduler.ExampleScheduler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -21,4 +23,12 @@ public class SchedulerConfig implements SchedulingConfigurer {
 
         taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
     }
+
+    /** NOTE: Scheduler 예시를 확인할 수 있는 ExampleScheduler 빈 주입 부분
+     *  NOTE: 아래 주석을 해제한 뒤 어플리케이션을 실행하면 동작
+     */
+//    @Bean
+//    public ExampleScheduler getExampleScheduler() {
+//        return new ExampleScheduler();
+//    }
 }
