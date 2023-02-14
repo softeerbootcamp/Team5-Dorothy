7683831f0e7ee63607b88e862808a9293b397a17
solchan98 <dev.sol0427@gmail.com>
2023-02-14 11:15:56 +0900

    feat(BE): NoticeReadServiceImpl Notice 단일 조회 구현 noticeId를 받아 단일 Notice를 조회하는 getNotice 메서드 구현 만약, notice가 존재하지 않다면 400 Bad Request 예외를 던짐

--- backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeReadServiceImpl.java
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeReadServiceImpl.java
@@ -1,11 +1,26 @@
 package dev.be.dorothy.notice.serivce;
 
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.notice.Notice;
+import dev.be.dorothy.notice.repository.NoticeRepository;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class NoticeReadServiceImpl implements NoticeReadService {
+    private final NoticeRepository noticeRepository;
+
+    public NoticeReadServiceImpl(NoticeRepository noticeRepository) {
+        this.noticeRepository = noticeRepository;
+    }
+
     @Override
     public NoticeResDto getNotice(Long noticeId) {
-        return null;
+        Notice notice = noticeRepository.findOne(noticeId)
+                .orElseThrow(() -> new BadRequestException("해당 공지사항이 존재하지 않습니다."));
+        // TODO: 현재 views 관련 기능 구현 전
+        return NoticeResDto.of(notice.getIdx(), notice.getTitle(), notice.getContent(), notice.getCreatedAt(), 0L);
     }
 
     @Override
