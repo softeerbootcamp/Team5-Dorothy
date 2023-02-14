d26c0cce7d858eb9e76612fb92b55c6f9833269c
solchan98 <dev.sol0427@gmail.com>
2023-02-14 11:06:11 +0900

    feat(BE): NoticeResDto 필드 추가 Notice의 idx 필드 추가

--- backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeResDto.java
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeResDto.java
@@ -9,12 +9,13 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NoticeResDto {
+    private final Long idx;
     private final String title;
     private final String content;
     private final LocalDateTime createdAt;
     private final Long views;
 
-    public static NoticeResDto of(String title, String content, LocalDateTime createdAt, Long views) {
-        return new NoticeResDto(title, content, createdAt, views);
+    public static NoticeResDto of(Long idx, String title, String content, LocalDateTime createdAt, Long views) {
+        return new NoticeResDto(idx, title, content, createdAt, views);
     }
 }
