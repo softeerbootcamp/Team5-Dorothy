cb91751fd83a90726a00aa6e4ed3eb253f46848b
pjs0418 <tony180322@gmail.com>
2023-02-14 16:59:30 +0900

    test(BE): 출석 요청 시간에 따른 출결 상태를 확인하기 위한 테스트 메서드 작성 정상적으로 출석하는 경우, 테스트하는 checkAttendanceTimeWhenPresent 메서드 작성 지각하는 경우, 테스트하는 checkAttendanceTimeWhenTardy 메서드 작성 결석하는 경우, 테스트하는 checkAttendanceTimeWhenAbsent 메서드 작성 출석 가능 시간 전에 출석 요청이 오는 경우, 예외 처리 테스트하는 checkAttendanceTimeTooEarly 메서드 작성

--- backend/src/test/java/dev/be/dorothy/attendance/service/AttendanceManagerServiceImplTest.java
+++ backend/src/test/java/dev/be/dorothy/attendance/service/AttendanceManagerServiceImplTest.java
@@ -1,5 +1,7 @@
 package dev.be.dorothy.attendance.service;
 
+import dev.be.dorothy.attendance.AttendanceType;
+import dev.be.dorothy.exception.BadRequestException;
 import dev.be.dorothy.track.Track;
 import dev.be.dorothy.track.service.TrackRetrieveService;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +56,74 @@ public class AttendanceManagerServiceImplTest {
         given(trackRetrieveService.getTrack(track.getIdx())).willReturn(track);
 
         assertThat(attendanceManagerServiceImpl.checkAttendanceLocation(track.getIdx(), x, y)).isFalse();
+    }
+
+    @Test
+    @DisplayName("출석 시간에 따른 출결 상태 테스트 - 출석 케이스")
+    void checkAttendanceTimeWhenPresent() {
+        Track track = new Track(
+                "hyundai",
+                ""
+        );
+        LocalTime time1 = LocalTime.of(10, 0);
+        LocalTime time2 = LocalTime.of(10, 9, 59);
+        LocalTime time3 = LocalTime.of(9, 30);
+
+        given(trackRetrieveService.getTrack(track.getIdx())).willReturn(track);
+
+        assertAll(
+                () -> assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time1)).isEqualTo(AttendanceType.PRESENT),
+                () -> assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time2)).isEqualTo(AttendanceType.PRESENT),
+                () -> assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time3)).isEqualTo(AttendanceType.PRESENT)
+        );
+    }
+
+    @Test
+    @DisplayName("출석 시간에 따른 출결 상태 테스트 - 지각 케이스")
+    void checkAttendanceTimeWhenTardy() {
+        Track track = new Track(
+                "hyundai",
+                ""
+        );
+        LocalTime time1 = LocalTime.of(10, 10);
+        LocalTime time2 = LocalTime.of(10, 29, 59);
+
+        given(trackRetrieveService.getTrack(track.getIdx())).willReturn(track);
+
+        assertAll(
+                () -> assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time1)).isEqualTo(AttendanceType.TARDY),
+                () -> assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time2)).isEqualTo(AttendanceType.TARDY)
+        );
+    }
+
+    @Test
+    @DisplayName("출석 시간에 따른 출결 상태 테스트 - 결석 케이스")
+    void checkAttendanceTimeWhenAbsent() {
+        Track track = new Track(
+                "hyundai",
+                ""
+        );
+        LocalTime time = LocalTime.of(10, 40);
+
+        given(trackRetrieveService.getTrack(track.getIdx())).willReturn(track);
+
+        assertThat(attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time)).isEqualTo(AttendanceType.ABSENT);
+    }
+
+    @Test
+    @DisplayName("출석 시간에 따른 출결 상태 테스트 - 출석 가능한 시간 전에 요청이 오는 케이스 예외 처리")
+    void checkAttendanceTimeTooEarly() {
+        Track track = new Track(
+                "hyundai",
+                ""
+        );
+        LocalTime time = LocalTime.of(9, 29, 59);
+
+        given(trackRetrieveService.getTrack(track.getIdx())).willReturn(track);
+
+        BadRequestException badRequestException = assertThrows(BadRequestException.class,
+                () -> attendanceManagerServiceImpl.checkAttendanceTime(track.getIdx(), time));
+
+        assertThat(badRequestException.getMessage()).isEqualTo("잘못된 출석 요청입니다.");
     }
 }
