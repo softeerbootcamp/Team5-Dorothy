f175cee709b0be3ce3d5dc9fc93df6f480201f0f
i5046821854 <o5046821854@gmail.com>
2023-02-14 16:30:27 +0900

    test(BE): PlaceRepositoryTest에 공간 예약 테스트 생성 더미 멤버와 공간을 생성한 후 공간 예약 신청에 대한 테스팅 - 성공 케이스만 한정하여 테스트

--- backend/src/test/java/dev/be/dorothy/reservation/repository/PlaceRepositoryTest.java
+++ backend/src/test/java/dev/be/dorothy/reservation/repository/PlaceRepositoryTest.java
@@ -1,14 +1,24 @@
 package dev.be.dorothy.reservation.repository;
 
+import dev.be.dorothy.member.Member;
+import dev.be.dorothy.member.MemberRole;
+import dev.be.dorothy.member.repository.MemberRepository;
 import dev.be.dorothy.reservation.Place;
 import dev.be.dorothy.reservation.service.PlaceResDto;
+import dev.be.dorothy.reservation.service.ReservationResDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
@@ -17,7 +27,10 @@ class PlaceRepositoryTest {
 
     @Autowired
     PlaceRepository placeRepository;
-    
+
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
     @DisplayName("공간 등록 기능 테스트")
     void placeRegisterTest(){
@@ -42,5 +55,36 @@ class PlaceRepositoryTest {
         //then
         Assertions.assertThat(allPlaces).hasSize(2);
     }
+
+    @Test
+    @DisplayName("공간 예약 신청 테스트 - 동시성 배제")
+    void reservePlaceTest(){
+
+        //given
+        Member member = Member.of(
+                "dorothy",
+                "abcd1234",
+                "2p7VxertGPCkNfnr",
+                "dorothy",
+                "dorothy@example.com",
+                MemberRole.MEMBER
+        );
+        memberRepository.save(member);
+
+        Place place = new Place("place1", "");
+        placeRepository.save(place);
+
+        //when
+        int reservationId = placeRepository.reservePlace(
+                member.getIdx(),
+                place.getIdx(),
+                LocalDateTime.now(),
+                LocalTime.now(),
+                LocalTime.now().plusMinutes(15),
+                false);
+
+        //then
+        Optional<ReservationResDto> reservation = placeRepository.findReservationById(reservationId);
+        assertThat(reservation).isNotNull();}
 
 }
