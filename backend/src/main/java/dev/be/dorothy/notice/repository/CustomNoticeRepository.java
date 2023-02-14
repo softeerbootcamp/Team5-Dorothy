f102e0bf3c38b8a141f6d6006b27d4619702e664
solchan98 <dev.sol0427@gmail.com>
2023-02-14 11:34:50 +0900

    test(BE): CustomNoticeRepository findAll 메서드 구현 Notice 전체를 조회하는 findAll 테스트 케이스 작성

--- backend/src/main/java/dev/be/dorothy/notice/repository/CustomNoticeRepository.java
+++ backend/src/main/java/dev/be/dorothy/notice/repository/CustomNoticeRepository.java
@@ -2,8 +2,10 @@ package dev.be.dorothy.notice.repository;
 
 import dev.be.dorothy.notice.Notice;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomNoticeRepository {
     Optional<Notice> findOne(Long noticeId);
+    List<Notice> findAll(boolean orderByCreatedAtDesc);
 }
