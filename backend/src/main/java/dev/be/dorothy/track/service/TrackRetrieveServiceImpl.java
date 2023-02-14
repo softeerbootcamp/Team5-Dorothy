ee0ab3e462b790930e24046c4409a3b28ee8f3ac
pjs0418 <tony180322@gmail.com>
2023-02-14 17:27:44 +0900

    chore(BE): 예외 메시지에 정보를 담지 않도록 수정

--- backend/src/main/java/dev/be/dorothy/track/service/TrackRetrieveServiceImpl.java
+++ backend/src/main/java/dev/be/dorothy/track/service/TrackRetrieveServiceImpl.java
@@ -25,13 +25,13 @@ public class TrackRetrieveServiceImpl implements TrackRetrieveService{
     public Track getTrack(Long trackIdx) {
         return trackRepository
                 .findById(trackIdx)
-                .orElseThrow(() -> new BadRequestException("존재하지 않는 트랙입니다."));
+                .orElseThrow(() -> new BadRequestException("잘못된 요청입니다."));
     }
 
     @Override
     public Long getTrackMemberIdx(Long memberIdx, Long trackIdx) {
         return trackRepository
                 .getTrackMemberIdx(memberIdx, trackIdx)
-                .orElseThrow(() -> new BadRequestException("해당 트랙에 찾고자 하는 멤버가 존재하지 않습니다."));
+                .orElseThrow(() -> new BadRequestException("잘못된 요청입니다."));
     }
 }
