c55c41796092b9fec26211d6155b235a7c00503a
solchan98 <dev.sol0427@gmail.com>
2023-02-14 10:57:08 +0900

    feat(BE): NoticeResDto 구현 Notice 응답 데이터를 담는 NoticeResDto 생성

---
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeResDto.java
@@ -0,0 +1,18 @@
+package dev.be.dorothy.notice.serivce;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeResDto {
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final Long views;
+
+    public static NoticeResDto of(String title, String content, LocalDateTime createdAt, Long views) {
+        return new NoticeResDto(title, content, createdAt, views);
+    }
+}
