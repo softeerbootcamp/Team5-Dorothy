63a33c6483a80770d2716b44f2b363e8fa80457b
pjs0418 <tony180322@gmail.com>
2023-02-15 15:38:13 +0900

    feat(BE): AttendanceRepository 메서드 구현 Attendance Idx 정보를 받아오는 getAttendanceIdx 메서드 구현

--- backend/src/main/java/dev/be/dorothy/attendance/repository/AttendanceRepository.java
+++ backend/src/main/java/dev/be/dorothy/attendance/repository/AttendanceRepository.java
@@ -1,7 +1,13 @@
 package dev.be.dorothy.attendance.repository;
 
 import dev.be.dorothy.attendance.Attendance;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AttendanceRepository extends CrudRepository<Attendance, Long> {
+    @Query("select idx from attendance where track_member_idx = :trackMemberIdx limit 1;")
+    Optional<Long> getAttendanceIdx(@Param("trackMemberIdx") Long trackMemberIdx);
 }
