308f90888996f50fb0d398645f67f52a861c58f0
i5046821854 <o5046821854@gmail.com>
2023-02-15 21:14:33 +0900

    feat(BE): PlaceReservationServiceImpl의 reservePlace 메소드 수정 동시성 제어를 위해, distributed Lock에 사용되는 키값 생성 로직 추가

--- backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationServiceImpl.java
+++ backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationServiceImpl.java
@@ -10,14 +10,26 @@ import java.time.LocalTime;
 public class PlaceReservationServiceImpl implements PlaceReservationService{
 
     private final PlaceRepository placeRepository;
-    public PlaceReservationServiceImpl(PlaceRepository placeRepository) {
+    private final PlaceReservationManagerImpl placeReservationManagerImpl;
+    private static final String RESERVATION_KEY = "RESERVATION_";
+
+    public PlaceReservationServiceImpl(PlaceRepository placeRepository, PlaceReservationManagerImpl placeReservationManagerImpl) {
         this.placeRepository = placeRepository;
+        this.placeReservationManagerImpl = placeReservationManagerImpl;
     }
 
     @Override
     public ReservationResDto reservePlace(Long memberIdx, Long placeIdx, LocalTime startTime) {
-        //TODO : 날짜와 시작 시간의 형식은 향후 수정할 예정
+        StringBuilder keyBuilder = new StringBuilder();
+        keyBuilder.append(RESERVATION_KEY)
+                .append(placeIdx)
+                .append("-")
+                .append(startTime);
+        if(!placeReservationManagerImpl.reservePlace(keyBuilder.toString(), memberIdx)){
+            throw new BadRequestException("이미 예약된 공간입니다.");
+        }
         Integer reservationID = placeRepository.reservePlace(memberIdx, placeIdx, LocalDateTime.now(), startTime, startTime.plusMinutes(15), false);
         return placeRepository.findReservationById(reservationID).orElseThrow(() -> new BadRequestException("입력 정보가 올바르지 않습니다."));
     }
+
 }
