e9971613016b0c9e4ae10e14a441cf90c56d9b89
pjs0418 <tony180322@gmail.com>
2023-02-15 22:43:57 +0900

    feat(BE): AttendanceController 메서드 구현 사용자 별로 당일 출결 현황 조회하는 readAttendanceByDay 메서드 구현

--- backend/src/main/java/dev/be/dorothy/attendance/controller/AttendanceController.java
+++ backend/src/main/java/dev/be/dorothy/attendance/controller/AttendanceController.java
@@ -1,8 +1,6 @@
 package dev.be.dorothy.attendance.controller;
 
-import dev.be.dorothy.attendance.service.AttendanceReqDto;
-import dev.be.dorothy.attendance.service.AttendanceResDto;
-import dev.be.dorothy.attendance.service.AttendanceService;
+import dev.be.dorothy.attendance.service.*;
 import dev.be.dorothy.common.CommonResponse;
 import dev.be.dorothy.member.MemberRole;
 import dev.be.dorothy.member.service.MemberResDto;
@@ -14,14 +12,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/attendance")
 public class AttendanceController {
     private final AttendanceService attendanceService;
+    private final AttendanceRetrieveService attendanceRetrieveService;
 
-    public AttendanceController(AttendanceService attendanceService) {
+    public AttendanceController(AttendanceService attendanceService, AttendanceRetrieveService attendanceRetrieveService) {
         this.attendanceService = attendanceService;
+        this.attendanceRetrieveService = attendanceRetrieveService;
     }
 
     @PostMapping("{trackIdx}")
@@ -40,6 +41,29 @@ public class AttendanceController {
                 attendanceReqDto
         );
         CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "출결 처리되었습니다.", attendanceResDto);
+
+        return new ResponseEntity<>(commonResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/day/{trackIdx}")
+    public ResponseEntity<CommonResponse> readAttendanceByDay(
+            @PathVariable("trackIdx") Long trackIdx) {
+        MemberDetail principal = (MemberDetail) ContextHolder.getContext().getPrincipal();
+        MemberResDto memberDto = principal.getMemberDto();
+
+        CommonResponse commonResponse = null;
+        if (MemberRole.valueOf(memberDto.getRole()) == MemberRole.MEMBER) {
+            AttendanceResDto attendanceResDto = attendanceRetrieveService.retrieveAttendanceByDayWhenMember(
+                    memberDto.getIdx(),
+                    trackIdx
+            );
+            commonResponse = new CommonResponse(HttpStatus.OK, "출결 현황 조회가 완료되었습니다.", attendanceResDto);
+        } else if (MemberRole.valueOf(memberDto.getRole()) == MemberRole.ADMIN) {
+            List<AttendanceRetrieveResDto> attendanceResDtoList = attendanceRetrieveService.retrieveAttendanceByDayWhenAdmin(
+                    trackIdx
+            );
+            commonResponse = new CommonResponse(HttpStatus.OK, "출결 현황 조회가 완료되었습니다.", attendanceResDtoList);
+        }
 
         return new ResponseEntity<>(commonResponse, HttpStatus.OK);
     }
