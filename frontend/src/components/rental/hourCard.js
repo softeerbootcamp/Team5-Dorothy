c6d730e427ff7f0f5ebb8ab8f0fd8b4e6085fb34
taehunjjang <hoonystory98@gmail.com>
2023-02-15 17:26:47 +0900

    feature: 공간대여 상세페이지 시간 선택 컴포넌트 분리

---
+++ frontend/src/components/rental/hourCard.js
@@ -0,0 +1,16 @@
+const hourCard = (hour) => {
+    return; /*html*/
+    `
+    <div class="time-line">
+        <h6 class="time-hour">10시</h6>
+            <div class="minute-card">
+                <figure class="time-box">00분</figure>
+                <figure class="time-box">15분</figure>
+                <figure class="time-box">30분</figure>
+                <figure class="time-box">45분</figure>
+            </div>
+    </div>
+    `;
+};
+
+export { hourCard };
