7f60f54d39a257fca3fa2a1740ac532a33e43692
pjs0418 <tony180322@gmail.com>
2023-02-15 17:47:14 +0900

    feat(BE): AttendanceRepository 메서드 구현 Member가 당일 출결 현황 조회할 때 필요한 getAttendanceByDayWhenMember 메서드 구현 Admin이 당일 트랙 멤버들의 출결 현황 조회할 때 필요한 getAttendanceByDayWhenAdmin 메서드 구현

--- backend/src/main/java/dev/be/dorothy/attendance/repository/AttendanceRepository.java
+++ backend/src/main/java/dev/be/dorothy/attendance/repository/AttendanceRepository.java
@@ -1,13 +1,21 @@
 package dev.be.dorothy.attendance.repository;
 
 import dev.be.dorothy.attendance.Attendance;
+import dev.be.dorothy.attendance.service.AttendanceResDto;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceRepository extends CrudRepository<Attendance, Long> {
     @Query("select idx from attendance where track_member_idx = :trackMemberIdx limit 1;")
     Optional<Long> getAttendanceIdx(@Param("trackMemberIdx") Long trackMemberIdx);
+
+    @Query("select date, time, type from attendance where track_member_idx = :trackMemberIdx;")
+    Optional<AttendanceResDto> getAttendanceByDayWhenMember(@Param("trackMemberIdx") Long trackMemberIdx);
+
+    @Query("select date, time, type from attendance inner join track_member on attendance.track_member_idx = track_member.idx where track_idx = :trackIdx and role = 'MEMBER' and is_deleted = 0;")
+    List<AttendanceResDto> getAttendanceByDayWhenAdmin(@Param("trackIdx") Long trackIdx);
 }
