c25c19653bf6371ec47b77f6629ed9e75be62fc8
miinjoo <minjoo9903@ewhain.net>
2023-02-15 17:56:54 +0900

    refactor: 로그인 성공시 함수 분리

--- frontend/src/apis/user.js
+++ frontend/src/apis/user.js
@@ -1,6 +1,7 @@
 import { stateModal } from '../components/modal';
 import { UserService } from './services/userservice';
 import { navigateTo } from '../router';
+import { qs } from '../utils/selector';
 
 // 로그아웃
 export const Logout = () => {
@@ -12,26 +13,13 @@ export const Logout = () => {
 export const GetUser = async (id, password) => {
     try {
         const response = await UserService.getUser(id, password);
-        document
-            .querySelector('.modal-place')
-            .insertAdjacentHTML(
-                'beforeend',
-                stateModal(response.data.code, response.data.message),
-            );
-        setTimeout(() => {
-            navigateTo('/track');
-        }, 1500);
+        succeedLogin(response.data.code, response.data.message);
         return Promise.resolve(response.data);
     } catch (error) {
-        document
-            .querySelector('.modal-place')
-            .insertAdjacentHTML(
-                'beforeend',
-                stateModal(
-                    error.response.data.code,
-                    error.response.data.message,
-                ),
-            );
+        qs('.modal-place').insertAdjacentHTML(
+            'beforeend',
+            stateModal(error.response.data.code, error.response.data.message),
+        );
         return Promise.reject(error.message, '로그인 실패');
     }
 };
@@ -52,46 +40,23 @@ export const PostUser = async (
             name,
             email,
         );
-        document
-            .querySelector('.modal-place')
-            .insertAdjacentHTML(
-                'beforeend',
-                stateModal(response.data.code, response.data.message),
-            );
-        setTimeout(() => {
-            navigateTo('/track');
-        }, 1500);
+        succeedLogin(response.data.code, response.data.message);
         return Promise.resolve(response.data);
     } catch (error) {
-        document
-            .querySelector('.modal-place')
-            .insertAdjacentHTML(
-                'beforeend',
-                stateModal(
-                    error.response.data.code,
-                    error.response.data.message,
-                ),
-            );
+        qs('.modal-place').insertAdjacentHTML(
+            'beforeend',
+            stateModal(error.response.data.code, error.response.data.message),
+        );
         return Promise.reject(error.message, '회원가입 실패');
     }
 };
 
-//공지 사항 전체 조회
-export const GetWholeNotices = async () => {
-    try {
-        const response = await UserService.getWholeNotice();
-    } catch (error) {
-        console.log(error);
-        return Promise.reject(error.message, '공지 전체 조회 실패');
-    }
-};
-
-//공지 사항 단일 조회
-export const GetNotice = async (id) => {
-    try {
-        const response = await UserService.getWholeNotice(id);
-    } catch (error) {
-        console.log(error);
-        return Promise.reject(error.message, '공지 단일 조회 실패');
-    }
+const succeedLogin = (code, message) => {
+    qs('.modal-place').insertAdjacentHTML(
+        'beforeend',
+        stateModal(code, message),
+    );
+    setTimeout(() => {
+        navigateTo('/track');
+    }, 1500);
 };
