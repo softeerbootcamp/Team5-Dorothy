991b13434ca531113f9311739e76e641a43f6757
solchan98 <dev.sol0427@gmail.com>
2023-02-16 18:33:24 +0900

    feat(BE): RedisDao 키 패턴으로 전체 조회 및 전체 삭제 메서드 구현 키 패턴으로 데이터 조회와 데이터 제거 기능 구현 테스트 케이스 작성

--- backend/src/test/java/dev/be/dorothy/redis/RedisDaoTest.java
+++ backend/src/test/java/dev/be/dorothy/redis/RedisDaoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = {RedisConfig.class, RedisDao.class})
@@ -45,5 +47,36 @@ public class RedisDaoTest {
         for (int idx = 0; idx < 1000; idx++) {
             redisDao.increment(key);
         }
+    }
+
+    @Test
+    @DisplayName("키 패턴으로 조회 테스트")
+    void keys() {
+        // given
+        redisDao.setValues("PAT_1_A", "A");
+        redisDao.setValues("PAT_1_B", "B");
+        redisDao.setValues("PAT_2_c", "C");
+
+        // when
+        Set<String> keys = redisDao.keys("PAT_1*");
+
+        // then
+        assertThat(keys.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("키 패턴으로 삭제 테스트")
+    void clearByKeyPattern() {
+        // given
+        redisDao.setValues("DEL_1_A", "A");
+        redisDao.setValues("DEL_1_B", "B");
+        redisDao.setValues("DEL_2_c", "C");
+
+        // when
+        redisDao.clear("DEL_1*");
+        Set<String> keys = redisDao.keys("DEL_1*");
+
+        // then
+        assertThat(keys.size()).isEqualTo(0);
     }
 }
