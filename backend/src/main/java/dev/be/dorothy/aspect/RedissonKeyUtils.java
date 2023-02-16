5f21857dfe14f7d757f12830a5877167663148b3
i5046821854 <o5046821854@gmail.com>
2023-02-16 17:23:01 +0900

    chore(BE): RedissonKeyUtils 클래스 명 변경 key값에 대한 여러 제반 작업들을 수행하는 메소드를 가지고 있으므로 util로 네이밍 변경

--- backend/src/main/java/dev/be/dorothy/aspect/RedissonKeyUtils.java
+++ backend/src/main/java/dev/be/dorothy/aspect/RedissonKeyUtils.java
@@ -4,10 +4,13 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-/**
- * SpelExpression으로 전달된 key값을 파싱하기 위한 클래스
- */
-public class RedissonKeyParser {
+import java.time.LocalTime;
+
+import static dev.be.dorothy.reservation.service.PlaceReservationServiceImpl.RESERVATION_KEY;
+
+public class RedissonKeyUtils {
+
+    //SpelExpression으로 전달된 key값을 파싱
     public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
         ExpressionParser parser = new SpelExpressionParser();
         StandardEvaluationContext context = new StandardEvaluationContext();
@@ -17,5 +20,14 @@ public class RedissonKeyParser {
         }
 
         return parser.parseExpression(key).getValue(context, Object.class);
+    }
+
+    public static String keyBuilder(Long placeIdx, LocalTime startTime){
+        StringBuilder keyBuilder = new StringBuilder();
+        keyBuilder.append(RESERVATION_KEY)
+                .append(placeIdx)
+                .append("-")
+                .append(startTime);
+        return keyBuilder.toString();
     }
 }
