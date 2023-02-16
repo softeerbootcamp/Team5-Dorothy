a0e21433491b3f9f9bdea2ff3022659460d928b7
solchan98 <dev.sol0427@gmail.com>
2023-02-16 11:38:56 +0900

    feat(BE): NoticeCreateServiceImpl cretea 메서드 구현 공지사항 생성 메서드 기능 구현

---
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeCreateServiceImpl.java
@@ -0,0 +1,33 @@
+package dev.be.dorothy.notice.serivce;
+
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.notice.Notice;
+import dev.be.dorothy.notice.repository.NoticeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeCreateServiceImpl implements NoticeCreateService {
+    private final NoticeRepository noticeRepository;
+
+    public NoticeCreateServiceImpl(NoticeRepository noticeRepository) {
+        this.noticeRepository = noticeRepository;
+    }
+
+    @Override
+    public NoticeResDto create(NoticeCreateDto noticeCreateDto) {
+        validateCreateData(noticeCreateDto);
+
+        Notice notice = Notice.of(noticeCreateDto.getTitle(), noticeCreateDto.getContent());
+        noticeRepository.save(notice);
+
+        return NoticeResDto.of(
+                notice.getIdx(), notice.getTitle(), notice.getContent(), notice.getUpdatedAt(), notice.getViews()
+        );
+    }
+
+    private void validateCreateData(NoticeCreateDto noticeCreateDto) {
+        if (noticeCreateDto.getTitle().isBlank()) {
+            throw new BadRequestException("요청 인자를 확인하세요.");
+        }
+    }
+}
