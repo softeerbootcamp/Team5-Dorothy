4d69b9dc44231db3ef43f96f3f93797812f68365
i5046821854 <o5046821854@gmail.com>
2023-02-16 18:02:40 +0900

    feat(BE): placeController 메소드에서 파라미터를 받는 방식을 변경 여러 시간에 대한 동시 선택을 대비하여 map 형태로 받은 뒤 이후 서비스 단에서 파싱하도록 변경

--- backend/src/main/java/dev/be/dorothy/reservation/controller/PlaceController.java
+++ backend/src/main/java/dev/be/dorothy/reservation/controller/PlaceController.java
@@ -3,16 +3,13 @@ package dev.be.dorothy.reservation.controller;
 import dev.be.dorothy.common.CommonResponse;
 import dev.be.dorothy.member.service.MemberResDto;
 import dev.be.dorothy.reservation.service.*;
-
 import dev.be.dorothy.security.context.ContextHolder;
 import dev.be.dorothy.security.context.MemberDetail;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/place")
@@ -34,8 +31,8 @@ public class PlaceController {
     }
 
     @PostMapping("")
-    public ResponseEntity<CommonResponse> placeAdd(@RequestParam("name") String name){
-        PlaceResDto newPlace = placeRegisterService.addPlace(name);
+    public ResponseEntity<CommonResponse> placeAdd(@RequestBody Map<String, Object> requestMap){
+        PlaceResDto newPlace = placeRegisterService.addPlace(requestMap.get("name").toString());
         CommonResponse commonResponse = new CommonResponse(HttpStatus.CREATED, "공간 등록이 완료되었습니다.", newPlace);
         return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
     }
@@ -50,9 +47,9 @@ public class PlaceController {
     @PostMapping("/reservation/{placeIdx}")
     public ResponseEntity<CommonResponse> reservePlace(
             @PathVariable Long placeIdx,
-            @RequestParam("time") @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime){
+            @RequestBody Map<String, Object> requestMap){
         MemberDetail memberDetail = (MemberDetail) ContextHolder.getContext().getPrincipal();
-        ReservationResDto appliedPlace = placeReservationService.reservePlace(memberDetail.getMemberDto().getIdx(),placeIdx, startTime);
+        List<ReservationResDto> appliedPlace = placeReservationService.reservePlace(memberDetail.getMemberDto().getIdx(),placeIdx, requestMap.get("data"));
         CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "공간 대여 신청이 완료되었습니다.", appliedPlace);
         return new ResponseEntity<>(commonResponse, HttpStatus.OK);
     }
