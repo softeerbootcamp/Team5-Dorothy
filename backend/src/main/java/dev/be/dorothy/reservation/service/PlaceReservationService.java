7dc8936091d933378d1e92336902278774ddbb7e
i5046821854 <o5046821854@gmail.com>
2023-02-16 13:53:58 +0900

    feat(BE): PlaceReservationService 메소드 추가 특정 공간에 대한 예약의 list를 가져오고, 한 멤버에 관한 예약 list를 가져오는 메소드를 정의

--- backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationService.java
+++ backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationService.java
@@ -1,9 +1,13 @@
 package dev.be.dorothy.reservation.service;
 
 import java.time.LocalTime;
+import java.util.List;
 
 public interface PlaceReservationService {
 
     ReservationResDto reservePlace(Long memberIdx, Long placeIdx, LocalTime startTime);
 
+    List<ReservationResDto> readReservationDetail(Long placeIdx);
+
+    List<ReservationResDto> readMyReservations(Long memberIdx);
 }
