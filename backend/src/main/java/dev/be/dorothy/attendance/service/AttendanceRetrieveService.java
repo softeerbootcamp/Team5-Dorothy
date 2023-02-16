4e0d7e07b10d4d147f634866faf20f5d0ed9e200
pjs0418 <33286894+pjs0418@users.noreply.github.com>
2023-02-16 18:21:26 +0900

    월간 출결 현황 조회를 위한 AttendanceRepository, AttendanceService, AttendanceController 구현 (#140)
    * chore(BE): AttendanceRepository 수정
    당일 출결 현황 데이터 가져오는 메서드 쿼리 문에서 당일인지 판단하는 쿼리 추가
    
    * feat(BE): AttendanceRepository 메서드 구현
    멤버가 월간 출결 현황 조회할 때 사용하는 getAttendanceByMonthWhenMember 메서드 구현
    
    * feat(BE): AttendanceRepository 메서드 구현
    관리자가 월간 출결 현황 조회할 때 사용하는 getAttendanceByMonthWhenAdmin 메서드 구현
    
    * feat(BE): AttendanceRepository 수정 및 메서드 추가 구현
    trackMemberIdx를 기준으로 attendanceIdx를 받아오는 메서드를 Attendance 객체를 받아오는 메서드로 수정
    출석 상태를 변경하는 attend 메서드 구현
    
    * feat(BE): 출석 방식이 변경됨에 따라 해당 로직 수정
    DB에 저장되어 있는 출결 상태가 ABSENT인 경우에만 출석 처리가 되도록 구현
    
    * chore(BE): AttendanceController 당일 출결 현황 조회하는 메서드에서 SUPER_ADMIN이 요청 보내는 경우, 403 예외 처리
    
    * feat(BE): AttendanceRetrieveService 구현
    월간 출결 현황 조회하는 retrieveAttendanceByMonth 메서드 구현
    
    * feat(BE): AttendanceController 메서드 구현
    월간 출결 현황 조회하는 readAttendanceByMonth 메서드 구현

--- backend/src/main/java/dev/be/dorothy/attendance/service/AttendanceRetrieveService.java
+++ backend/src/main/java/dev/be/dorothy/attendance/service/AttendanceRetrieveService.java
@@ -1,8 +1,11 @@
 package dev.be.dorothy.attendance.service;
 
+import dev.be.dorothy.member.MemberRole;
+
 import java.util.List;
 
 public interface AttendanceRetrieveService {
     AttendanceResDto retrieveAttendanceByDayWhenMember(Long memberIdx, Long trackIdx);
     List<AttendanceRetrieveResDto> retrieveAttendanceByDayWhenAdmin(Long trackIdx);
+    List<AttendanceResDto> retrieveAttendanceByMonth(Long memberIdx, Long trackIdx, MemberRole role);
 }
--- backend/src/main/java/dev/be/dorothy/attendance/service/AttendanceRetrieveServiceImpl.java
+++ backend/src/main/java/dev/be/dorothy/attendance/service/AttendanceRetrieveServiceImpl.java
@@ -1,6 +1,8 @@
 package dev.be.dorothy.attendance.service;
 
 import dev.be.dorothy.attendance.repository.AttendanceRepository;
+import dev.be.dorothy.exception.ForbiddenException;
+import dev.be.dorothy.member.MemberRole;
 import dev.be.dorothy.track.service.TrackRetrieveService;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +28,20 @@ public class AttendanceRetrieveServiceImpl implements AttendanceRetrieveService 
     @Override
     public List<AttendanceRetrieveResDto> retrieveAttendanceByDayWhenAdmin(Long trackIdx) {
         return attendanceRepository.getAttendanceByDayWhenAdmin(trackIdx);
+    }
+
+    @Override
+    public List<AttendanceResDto> retrieveAttendanceByMonth(Long memberIdx, Long trackIdx, MemberRole role) {
+        if(role == MemberRole.SUPER_ADMIN) {
+            throw new ForbiddenException("잘못된 접근입니다.");
+        }
+
+        if(role == MemberRole.MEMBER) {
+            Long trackMemberIdx = trackRetrieveService.getTrackMemberIdx(memberIdx, trackIdx);
+
+            return attendanceRepository.getAttendanceByMonthWhenMember(trackMemberIdx);
+        }
+
+        return attendanceRepository.getAttendanceByMonthWhenAdmin(trackIdx);
     }
 }
