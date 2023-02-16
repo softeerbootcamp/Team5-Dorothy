9bf6a6c75b4ef3ff96b773a10b02d4b368bb0087
i5046821854 <o5046821854@gmail.com>
2023-02-17 00:21:09 +0900

    feat(BE): PlaceReservationManager에 실패 케이스를 핸들링하는 메소드 추가 모두 실패 시, 일부 실패 시 모두 처리

--- backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationManagerImpl.java
+++ backend/src/main/java/dev/be/dorothy/reservation/service/PlaceReservationManagerImpl.java
@@ -1,8 +1,12 @@
 package dev.be.dorothy.reservation.service;
 
 import dev.be.dorothy.aspect.EnableLock;
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.exception.PartialContentException;
 import dev.be.dorothy.redis.RedisDao;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +25,15 @@ public class PlaceReservationManagerImpl implements PlaceReservationManager{
         }
         redisDao.setValues(key, String.valueOf(memberIdx));
         return true;
+    }
+
+    public void findFailReservation(int paramLength, int failLength, List<ReservationResDto> resultList){
+        if(paramLength == failLength){
+            throw new BadRequestException("공간 예약에 실패하였습니다");
+        }
+        if(failLength > 0) {
+            String resultMsg = String.format("%d개의 요청 중 %d개의 요청에 실패하였습니다", paramLength, paramLength - failLength);
+            throw new PartialContentException(resultMsg, resultList);
+        }
     }
 }
