9f7fef67d76784414d5a5cdc5a56c8712e5c13fa
solchan98 <dev.sol0427@gmail.com>
2023-02-16 11:38:18 +0900

    test(BE): NoticeCreateService create 메서드 테스트 케이스 작성 타이틀이 비어있는 상태로 공지사항 생성 요청시 400 예외를 던지는지 확인하는 테스트 케이스 작성

--- backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeCreateDto.java
+++ backend/src/main/java/dev/be/dorothy/notice/serivce/NoticeCreateDto.java
@@ -1,8 +1,10 @@
 package dev.be.dorothy.notice.serivce;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class NoticeCreateDto {
     private String title;
     private String content;
---
+++ backend/src/test/java/dev/be/dorothy/notice/service/NoticeCreateServiceTest.java
@@ -0,0 +1,35 @@
+package dev.be.dorothy.notice.service;
+
+import dev.be.dorothy.exception.BadRequestException;
+import dev.be.dorothy.notice.serivce.NoticeCreateDto;
+import dev.be.dorothy.notice.serivce.NoticeCreateServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("NoticeCreateService Test")
+@ExtendWith(MockitoExtension.class)
+public class NoticeCreateServiceTest {
+
+    @InjectMocks
+    NoticeCreateServiceImpl noticeCreateService;
+
+    @Test
+    @DisplayName("빈 타이틀로 공지사항 생성 시, 400 예외를 던지는지 테스트")
+    void createWithInvalidData() {
+        // given
+        NoticeCreateDto noticeCreateDto = new NoticeCreateDto("", "all pass");
+
+        // when then
+        BadRequestException exception = assertThrows(
+                BadRequestException.class,
+                () -> noticeCreateService.create(noticeCreateDto)
+        );
+        assertThat(exception.getMessage()).isEqualTo("요청 인자를 확인하세요.");
+    }
+}
