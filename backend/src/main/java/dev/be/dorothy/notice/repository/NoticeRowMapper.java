6039bfcca590682b9c61e94ab317d9eae4db186e
solchan98 <dev.sol0427@gmail.com>
2023-02-16 11:35:17 +0900

    feat(BE): NoticeRowMapper 수정 Notice 도메인에 views 필드 추가로 인한 views 필드 추가

--- backend/src/main/java/dev/be/dorothy/notice/repository/NoticeRowMapper.java
+++ backend/src/main/java/dev/be/dorothy/notice/repository/NoticeRowMapper.java
@@ -10,11 +10,12 @@ public class NoticeRowMapper implements RowMapper<Notice> {
     @Override
     public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Notice(
-                Long.valueOf(rs.getString("idx")),
+                rs.getLong("idx"),
                 rs.getString("title"),
                 rs.getString("content"),
                 rs.getTimestamp("created_at").toLocalDateTime(),
                 rs.getTimestamp("updated_at").toLocalDateTime(),
+                rs.getLong("views"),
                 Boolean.parseBoolean(rs.getString("is_deleted"))
         );
     }
