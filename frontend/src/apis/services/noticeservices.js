d6c4a6f0eb32c31db361db2dfd85694edcac948b
miinjoo <minjoo9903@ewhain.net>
2023-02-17 13:16:17 +0900

    fix: 배포를 위한 url수정

--- frontend/src/apis/services/noticeservices.js
+++ frontend/src/apis/services/noticeservices.js
@@ -5,17 +5,17 @@ import { http } from '../http.js';
 const NoticeService = {
     // 전체 공지 사항 조회
     getAllNotices: async () => {
-        const response = await http.get('/api/notice/list');
+        const response = await http.get('/notice/list');
         return response.data.data;
     },
     // 단일 공지 사항 조회
     getNotice: async (id) => {
-        const response = await http.get(`/api/notice/${id}`);
+        const response = await http.get(`/notice/${id}`);
         return response.data.data;
     },
     // 공지 사항 생성
     postNotice: async (title, content) => {
-        const response = await http.post(`/api/notice`, { title, content });
+        const response = await http.post(`/notice`, { title, content });
         return response.data.data;
     },
 };
--- frontend/src/apis/services/trackservices.js
+++ frontend/src/apis/services/trackservices.js
@@ -3,27 +3,27 @@ import { http } from '../http.js';
 const TrackService = {
     //트랙 생성
     postTrack: async (trackname) => {
-        const response = await http.post(`/api/track?name=${trackname}`, {});
+        const response = await http.post(`/track?name=${trackname}`, {});
         return response;
     },
 
     //나의 트랙 조회
     getTrack: async () => {
-        const response = await http.get(`/api/track/list`);
+        const response = await http.get(`/track/list`);
         return response.data.data;
     },
 
     //트랙 가입
     postTrackMember: async (trackIdx, joinCode) => {
         const response = await http.post(
-            `/api/track/join/${trackIdx}?joinCode=${joinCode}`,
+            `/track/join/${trackIdx}?joinCode=${joinCode}`,
         );
         return response;
     },
 
     //현재 트랙 초대 코드 조회
     getTrackCode: async (currentIdx) => {
-        const response = await http.get(`/api/track/code/${currentIdx}`);
+        const response = await http.get(`/track/code/${currentIdx}`);
         return response.data;
     },
 };
