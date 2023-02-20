b3e23a05813317ca1ff734959aec5db2d8cd7573
i5046821854 <o5046821854@gmail.com>
2023-02-20 16:44:05 +0900

    feat(BE): ReservationResDto 필드 변경 endTime도 받을 수 있도록 변경

--- backend/src/main/java/dev/be/dorothy/reservation/service/ReservationResDto.java
+++ backend/src/main/java/dev/be/dorothy/reservation/service/ReservationResDto.java
@@ -1,17 +1,27 @@
 package dev.be.dorothy.reservation.service;
 
+import java.time.LocalTime;
+
 public class ReservationResDto {
 
     private final Long placeIdx;
     private final String startTime;
+    private String endTime;
 
-    public ReservationResDto(Long placeIdx, String startTime) {
+    public ReservationResDto(Long placeIdx, String startTime, String endTime) {
         this.placeIdx = placeIdx;
         this.startTime = startTime.substring(0, startTime.lastIndexOf(":"));
+        this.endTime = endTime.substring(0, endTime.lastIndexOf(":"));
     }
 
-    public static ReservationResDto of(Long placeIdx, String time) {
-        return new ReservationResDto(placeIdx, time);
+    private ReservationResDto(Long placeIdx, LocalTime startTime, LocalTime endTime) {
+        this.placeIdx = placeIdx;
+        this.startTime = startTime.toString();
+        this.endTime = endTime.toString();
+    }
+
+    public static ReservationResDto of(Long placeIdx, LocalTime startTime, LocalTime endTime) {
+        return new ReservationResDto(placeIdx, startTime, endTime);
     }
 
     public Long getPlaceIdx() {
@@ -20,5 +30,13 @@ public class ReservationResDto {
 
     public String getStartTime() {
         return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
     }
 }
